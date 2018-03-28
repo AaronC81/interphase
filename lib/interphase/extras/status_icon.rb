c3b8165a008c326a0c7fc71279ed67cccfa9f722
Aaron Christiansen <aaronc20000@gmail.com>
2018-03-28 08:54:43 +0100

    Refactor StatusIcon

--- lib/interphase/extras/status_icon.rb
+++ lib/interphase/extras/status_icon.rb
@@ -5,8 +5,6 @@ require 'gtk2'
 module Interphase
   # A tray status icon.
   class StatusIcon < UiObject
-    attr_reader :gtk_instance
-
     def initialize(tooltip)
       @gtk_instance = Gtk::StatusIcon.new
       self.tooltip = tooltip
@@ -16,13 +14,8 @@ module Interphase
       on('activate', &block)
     end
 
-    def tooltip
-      @gtk_instance.tooltip
-    end
-
-    def tooltip=(value)
-      @gtk_instance.tooltip = value
-    end
+    gtk_passthrough :tooltip, 'Get this icon\'s tooltip.'
+    gtk_passthrough :tooltip=, 'Set this icon\'s tooltip.'
 
     def icon=(icon)
       raise ArgumentError, 'Must be a StockIcon' unless icon.is_a? StockIcon
